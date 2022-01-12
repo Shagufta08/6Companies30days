29208709304bf48888fe16da514cbcd0ca0db685
Shagufta Iqbal <61472230+Shagufta08@users.noreply.github.com>
2022-01-13 01:14:51 +0530

    Create Nuts and Bolts Problem.cpp

---
+++ Amazon/Nuts and Bolts Problem.cpp
@@ -0,0 +1,64 @@
+// Nuts and Bolts Problem
+// Problem link: https://practice.geeksforgeeks.org/problems/nuts-and-bolts-problem0431/1#
+
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+
+	void matchPairs(char nuts[], char bolts[], int n) {
+	    // code here
+	    char arr[] = {'!','#','$','%','&','*','@','^','~'};
+        unordered_set<char>s;
+        
+        for(int i=0;i<n;i++)    s.insert(nuts[i]);
+        
+        int i = 0;
+        for(auto c:arr){
+            if(s.find(c)!=s.end()){
+                nuts[i]=c;
+                bolts[i]=c;
+                i++;
+            }
+        }
+	    
+	}
+
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        char nuts[n], bolts[n];
+        for (int i = 0; i < n; i++) {
+            cin >> nuts[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> bolts[i];
+        }
+        Solution ob;
+        ob.matchPairs(nuts, bolts, n);
+        for (int i = 0; i < n; i++) {
+            cout << nuts[i] << " ";
+        }
+        cout << "\n";
+        for (int i = 0; i < n; i++) {
+            cout << bolts[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
